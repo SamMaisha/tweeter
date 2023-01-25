bb9bbad19aa788b317fd49a7be0108a0ee48a9ee
Sam Maisha <sam.maisha99@gmail.com>
2023-01-25 11:58:30 +0000

    add temporary test code to make sure createTweetElemement is functioning as expected

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,20 @@
-$(document).ready(function() {
-  const $form = $('tweet-form')
-  const $inputTweet = $('#tweet-text'); // can also directly reference tweet-text 
+$(document).ready(function () {
+  const $inputTweet = $("#tweet-text"); // can also directly reference tweet-text
 
-  $inputTweet.on("input", function(event) {
+  $inputTweet.on("input", function (event) {
     // number of characters in tweet
     const tweetCharacterCount = $(this).val().length;
-    // number of characters remaining 
+    // number of characters remaining
     const tweetCharactersLeft = 140 - tweetCharacterCount;
     // accessing the counter element
-    const tweetCounter = $(this).siblings('div').children('.counter');
-    // updating counter with characters left in tweet 
+    const tweetCounter = $(this).siblings("div").children(".counter");
+    // updating counter with characters left in tweet
     tweetCounter.text(tweetCharactersLeft);
     // add class to turn counter red when tweet goes above character limit
     if (tweetCharactersLeft < 0) {
-      $('.counter').addClass("counter-warning");
+      $(".counter").addClass("counter-warning");
     } else {
-      $('.counter').removeClass("counter-warning");
+      $(".counter").removeClass("counter-warning");
     }
   });
-})
-
-
+});
