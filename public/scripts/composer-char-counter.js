78cfd90982b558a944e0615874c50ace3f53d757
Sam Maisha <sam.maisha99@gmail.com>
2023-01-25 04:34:10 +0000

    add logic to turn the counter button red if character limit is exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,21 @@ $(document).ready(function() {
   const $form = $('tweet-form')
   const $inputTweet = $('#tweet-text'); // can also directly reference tweet-text 
 
-
   $inputTweet.on("input", function(event) {
+    // number of characters in tweet
     const tweetCharacterCount = $(this).val().length;
+    // number of characters remaining 
     const tweetCharactersLeft = 140 - tweetCharacterCount;
-    console.log(tweetCharactersLeft);
-    
+    // accessing the counter element
     const tweetCounter = $(this).siblings('div').children('.counter');
-  
+    // updating counter with characters left in tweet 
     tweetCounter.text(tweetCharactersLeft);
-    
+    // add class to turn counter red when tweet goes above character limit
+    if (tweetCharactersLeft < 0) {
+      $('.counter').addClass("counter-warning");
+    } else {
+      $('.counter').removeClass("counter-warning");
+    }
   });
 })
 
