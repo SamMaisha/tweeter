24ddda62d5a38e57df588c75bab5aeb656a24717
Sam Maisha <sam.maisha99@gmail.com>
2023-01-27 20:24:44 +0000

    invoke renderTweets() when ajax get call is successful

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -63,39 +63,14 @@ const loadTweets = function() {
     type: "GET",
     dataType: "json",
     success: (data) => {
-      console.log("the request was successful. Here is the data:", data)
+      console.log("The request was successful, and here is the data:", data);
+      renderTweets(data);
     },
     error: (error) => {
-      console.error("There was an error:", error)
+      console.error("An error occurred, ", error);
     }
   })
 }
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
 
 
 $(document).ready(function () {
